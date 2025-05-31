e6bbfb92ceb0f47a6d1d86e8024ad18498c3f100
omarmnmk <omar.namnakani@gmail.com>
2025-06-01 00:59:24 +0100

    fixed port number

--- app.py
+++ app.py
@@ -228,4 +228,5 @@ if __name__ == "__main__":
     threading.Thread(target=background_setup, daemon=True).start()
 
     # Start the Flask server
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
+
